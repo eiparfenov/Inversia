208c18059798fbedcb3e27c262c22ae10fdf9c27
eiparfenov <62956146+eiparfenov@users.noreply.github.com>
2022-03-13 16:31:49 +0300

    Add shadow collider script

---
+++ Scripts/ShadowCollider.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+
+public class ShadowCollider : MonoBehaviour
+{
+    [SerializeField] private GameObject tempMarker;
+    public void RecalculateShadow(Vector3[] meshVertexesPositions, Vector3 lightSourcePosition)
+    {
+        Vector3[] pointsProjections = ProjectPointsOnPlane(meshVertexesPositions, lightSourcePosition);
+        TempDisplay(pointsProjections);
+    }
+
+    private Vector3[] ProjectPointsOnPlane(Vector3[] meshVertexesPositions, Vector3 lightSourcePosition)
+    {
+        Vector3[] meshVertexesLocalPositions = new Vector3[meshVertexesPositions.Length];
+        for (int i = 0; i < meshVertexesPositions.Length; i++)
+        {
+            meshVertexesLocalPositions[i] = transform.InverseTransformPoint(meshVertexesPositions[i]);
+        }
+
+        Vector3 lightSourceLocalPosition = transform.InverseTransformPoint(lightSourcePosition);
+
+
+        Vector3[] pointsOnPlane = new Vector3[meshVertexesPositions.Length];
+
+        Vector3 lightSourceParallelProjection = lightSourceLocalPosition;
+        lightSourceParallelProjection.z = 0;
+
+        for (int i = 0; i < meshVertexesLocalPositions.Length; i++)
+        {
+            Vector3 currentPoint = meshVertexesLocalPositions[i];
+            Vector3 currentPointParallelProjection = currentPoint;
+            currentPointParallelProjection.z = 0;
+
+            pointsOnPlane[i] = lightSourceParallelProjection + currentPointParallelProjection *
+                (lightSourceLocalPosition.z / (lightSourceLocalPosition.z - meshVertexesLocalPositions[i].z));
+        }
+
+        return pointsOnPlane;
+    }
+
+    private void TempDisplay(Vector3[] pointsToDisplay)
+    {
+        for (int childNumber = 0; childNumber < transform.childCount; childNumber++)
+        {
+            Destroy(transform.GetChild(0).gameObject);
+        }
+
+        foreach (var point in pointsToDisplay)
+        {
+            Instantiate(tempMarker, point, Quaternion.identity, transform);
+        }
+    }
+    
+}
