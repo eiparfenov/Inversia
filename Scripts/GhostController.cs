0f6797e629b457f97f5714a4ad65c20f38462f9b
eiparfenov <62956146+eiparfenov@users.noreply.github.com>
2022-03-14 14:27:13 +0300

    add blocks controls

---
+++ Scripts/GhostController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostController : MonoBehaviour
+{
+    [SerializeField] [HideInInspector] private Ghost ghost;
+    public Ghost AppliedGhost => ghost;
+
+    private void Update()
+    {
+        
+    }
+}
---
+++ Scripts/Hand.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Hand : MonoBehaviour
+{
+    [SerializeField] private SteamVR_Action_Boolean trigger;
+
+    [SerializeField] private SteamVR_Behaviour_Pose controller;
+
+    [SerializeField] private FixedJoint fixedJoint;
+
+    [SerializeField] private List<Interactable> interactables = new List<Interactable>();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        controller = GetComponent<SteamVR_Behaviour_Pose>();
+        fixedJoint = GetComponent<FixedJoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trigger.GetStateDown(controller.inputSource))
+        {
+            Interactable interactable = GetClosest();
+
+            if (!interactable)
+                return;
+
+            fixedJoint.connectedBody = interactable.GetComponent<Rigidbody>();
+        }
+        if (trigger.GetStateUp(controller.inputSource))
+        {
+            fixedJoint.connectedBody = null;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Enter");
+        Interactable interactable = other.gameObject.GetComponentInParent<Interactable>();
+        if (interactable && !interactables.Contains(interactable))
+        {
+            interactables.Add(interactable);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Interactable interactable = other.gameObject.GetComponentInParent<Interactable>();
+        if (interactable && interactables.Contains(interactable))
+        {
+            interactables.Remove(interactable);
+        }
+    }
+
+    private Interactable GetClosest()
+    {
+        if (interactables.Count == 0)
+            return null;
+
+        float minDist = (transform.position - interactables[0].transform.position).magnitude;
+        Interactable closest = interactables[0];
+
+        foreach (var interactable in interactables)
+        {
+            float newDist = (transform.position - interactable.transform.position).magnitude;
+            if (newDist < minDist)
+            {
+                minDist = newDist;
+                closest = interactable;
+            }
+        }
+
+        return closest;
+    }
+}
